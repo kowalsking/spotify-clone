eecd980eef1ed7855f3222ecc085d41b9dcc86b8
kowalsking <kovvalsking@gmail.com>
2022-01-12 00:18:27 +0200

    Auth form page created

---
+++ pages/singin.tsx
@@ -0,0 +1,9 @@
+import AuthForm from '../components/authForm';
+
+const Singin = () => {
+  return <AuthForm mode="singin" />
+}
+
+Singin.authPage = true
+
+export default Singin
---
+++ pages/singup.tsx
@@ -0,0 +1,9 @@
+import AuthForm from '../components/authForm';
+
+const Singup = () => {
+  return <AuthForm mode="singup" />
+}
+
+Singup.authPage = true
+
+export default Singup
